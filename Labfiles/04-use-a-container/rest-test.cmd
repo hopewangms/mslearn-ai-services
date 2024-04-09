fc1f3c849de69040b04027c49bb66e3969e3f734
Hope Wang <hopewang@microsoft.com>
2024-04-09 13:41:02 -0700

    Learn Azure AI Service

---
+++ Labfiles/04-use-a-container/rest-test.cmd
@@ -0,0 +1,2 @@
+curl -X POST "http://dnsnamehope.hkhkcjc0aqf2f4f5.westus3.azurecontainer.io:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
+curl -X POST "http://20.171.223.27:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
---
+++ README.md
@@ -0,0 +1,4 @@
+# mslearn-ai-services
+Lab files for Azure AI Services modules
+
+This is my own learning repository to learn Azure AI Services
